7355bdf2adbecb017fc6f8123a9014b8edc5d0d8
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 18:13:28 +0530

    Testing get Data

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -1,15 +1,39 @@
-// Import packages
+require("dotenv").config(); // Load environment variables from .env file
+
+// Now you can access environment variables using process.env
+
+const { connectDB } = require("./models/User");
 const express = require("express");
-
-// Middlewares
+const cors = require("cors");
+// const userRoute = require("./route/userRoute");
 const app = express();
-app.use(express.json());
-
-// Routes
+app.use(express.json({ extended: false }));
+app.use(cors());
+const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.json("hello");
 });
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://mern-product-frontend.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+// app.use("/", userRoute);
+
+// Routes
+
+app.listen(PORT, () => {
+  console.log(`Server Started ${PORT}`);
+  connectDB();
+});
+
+// Start the server
