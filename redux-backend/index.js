0326eb17798c989a3071279eef9fd842f750bc0f
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 17:40:19 +0530

    Updating api frontend

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -9,18 +9,18 @@ const userRoute = require("./route/userRoute");
 const app = express();
 const PORT = process.env.PORT;
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://mern-product-frontend.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://mern-product-frontend.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use(express.json({ extended: false }));
-// app.use(cors());
+app.use(cors());
 app.use("/", userRoute);
 
 // Routes
