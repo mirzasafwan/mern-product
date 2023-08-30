06399dcf1385d69833943f353d4d26d385bfbdbb
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 23:08:33 +0530

    Applying Port on Appropriate place

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -3,12 +3,11 @@ require("dotenv").config(); // Load environment variables from .env file
 // Now you can access environment variables using process.env
 const express = require("express");
 const cors = require("cors");
-
+const PORT = process.env.PORT;
 const { connectDB } = require("./models/User");
 const userRoute = require("./route/userRoute");
 const app = express();
 app.use(express.json({ extended: false }));
-const PORT = process.env.PORT;
 
 app.use(cors());
 app.use("/", userRoute);
