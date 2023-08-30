3e92d0d350d2f67a1ef341bccd9f23604572cfa4
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 18:22:57 +0530

    Testing get Data

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -5,7 +5,7 @@ require("dotenv").config(); // Load environment variables from .env file
 const { connectDB } = require("./models/User");
 const express = require("express");
 const cors = require("cors");
-// const userRoute = require("./route/userRoute");
+const userRoute = require("./route/userRoute");
 const app = express();
 app.use(express.json({ extended: false }));
 app.use(cors());
