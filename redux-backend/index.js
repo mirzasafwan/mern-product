47311aadfcf1e24209f1cd8e0c65d3e17277e56d
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 18:35:33 +0530

    Testing get Data

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -1,4 +1,11 @@
 require("dotenv").config(); // Load environment variables from .env file
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 // Now you can access environment variables using process.env
 
