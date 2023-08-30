40b9fd500cf3371b8182592c20fc747f34ee8abf
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-30 17:30:24 +0530

    Updating api frontend

--- redux-backend/index.js
+++ redux-backend/index.js
@@ -4,19 +4,20 @@ require("dotenv").config(); // Load environment variables from .env file
 
 const { connectDB } = require("./models/User");
 const express = require("express");
-
 const cors = require("cors");
 const userRoute = require("./route/userRoute");
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(
-  cors({
-    origin: ["https://mern-product-frontend.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://mern-product-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json({ extended: false }));
 // app.use(cors());
